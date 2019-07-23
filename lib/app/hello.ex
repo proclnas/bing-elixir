5d9a4eb9167c48c13e60070e56ace12901a65d35
Rodrigo Nas <proclnas@gmail.com>
2019-07-23 02:09:08 -0300

    Bing poc
    - Add Search with HTTPotion
    - Parse links with Floki
    - Add save_buf/1 to save local file
    - Ignore .elixir_ls build folder

--- lib/app/hello.ex
+++ lib/app/hello.ex
@@ -1,5 +1,6 @@
 defmodule App.Hello do
   @limit_page Application.get_env(:app, :limit_page)
+  @blacklist ~r/bing|microsoft|search|javascript.+|^\/|^#/
 
   def start(file_name \\ "words.txt") do
     File.stream!(file_name)
@@ -10,11 +11,8 @@ defmodule App.Hello do
   def bing_search(word, ptr \\ 1) do
     IO.puts("[#{ptr}/#{@limit_page}] Searching: #{word}")
 
-    "http://www.bing.com/search?q=#{word}&count=50&first=#{ptr}"
-    |> HTTPotion.get
-    |> IO.puts
-    #|> @html
-    #|> Enum.map(&save_buf/1)
+    HTTPotion.get("http://www.bing.com/search?q=#{word}&count=50&first=#{ptr}")
+    |> process_response
 
     cond do
       ptr < @limit_page -> bing_search(word, ptr + 10)
@@ -23,12 +21,25 @@ defmodule App.Hello do
   end
 
   def extract_links(html_content) do
-    Floki.find(html_content, "a")
-    #html_content
-    #|> Floki.find("a")
-    #|> Floki.attribute("href")
+    html_content
+    |> Floki.parse
+    |> Floki.find("a")
+    |> Floki.attribute("href")
+    |> Enum.filter(fn(x) -> Regex.match?(@blacklist, x) == false end)
+    |> Enum.map(&save_buf/1)
   end
 
-  def save_buf(content) do
+  def process_response(%HTTPotion.Response{status_code: status_code, body: body} = resp) do
+    cond do
+      status_code == 200 -> extract_links(body)
+      true -> {:error, resp}
+    end
+  end
+
+  def save_buf(content, output \\ "output.txt") do
+    case File.write(output, "#{content}\n", [:append]) do
+      :ok -> IO.puts("[+] Uri saved to #{output}")
+      _ -> IO.puts("[-] Error on save uri")
+    end
   end
 end
